a21e539f3ab5976edc35701ea6e59d5e04d42072
Chase Brasfield <chasebrasfield@gmail.com>
2016-05-18 01:30:29 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.skills').hide();
+    $('.experience').hide();
+    $('.contact').hide();
+});
+
+function showAbout() {;
+        $('.content').hide();
+        $('.about').show();
+}
+
+function showSkills() {
+        $('.content').hide();
+        $('.skills').show();
+}
+
+function showExp() {
+        $('.content').hide();
+        $('.experience').show();
+}
+
+function showContact() {
+        $('.content').hide();
+        $('.contact').show();
+}
+
